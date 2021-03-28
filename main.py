48e38576bba730ff9349b956c3639979f218a555
pelaajahacks <ronttipontti.sundstrom2@gmail.com>
2021-03-28 16:49:51 +0300

    window

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# initialize
+pygame.init()
+
+# create screen
+screen = pygame.display.set_mode((1080, 720))
+
+running = True
+while running:
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
